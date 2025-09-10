a5e0e4c5b50721e3995b2d4b4f12d9036939b3af
jack <jl1171881690@gmail.com>
2025-09-09 20:08:13 -0400

    refactor codes

--- main.py
+++ main.py
@@ -106,38 +106,45 @@ def login_utoronto_and_get_tokens():
         driver.quit()
 
 
-cookies_dict, token = login_utoronto_and_get_tokens()
+def get_courses_info(only_course_code=False) -> list:
+    course_codes_list = []
+    cookies_dict, token = login_utoronto_and_get_tokens()
 
+    TIMETABLEAPI = "https://acorn.utoronto.ca/sws/rest/timetable/viewTimetable"
+    headers = {
+        "Content-Type": "application/json",
+        "X-XSRF-TOKEN": token,
+    }
 
-url = "https://acorn.utoronto.ca/sws/rest/timetable/viewTimetable"
-
-headers = {
-    "Content-Type": "application/json",
-    "X-XSRF-TOKEN": token,
-}
-
-registration_infos_response = requests.get(
-    url="https://acorn.utoronto.ca/sws/rest/enrolment/current-registrations",
-    cookies=cookies_dict,
-)
-registration_infos = registration_infos_response.text
-registration_info_post_code = json.loads(registration_infos)[0].get("post").get("code")
-registration_info_session_code = json.loads(registration_infos)[0].get("sessionCode")
-data = {
-    "code": registration_info_session_code,
-    "posts": [
-        {
-            "code": registration_info_post_code,
-        }
-    ],
-}
-all_courses = {}
-response = requests.post(
-    url, headers=headers, data=json.dumps(data), cookies=cookies_dict
-)
-response = json.loads(response.text)
-courses_infos = (
-    response.get("schedule").get("registrations")[0].get("academicActivitiesList")
-)
-for course in courses_infos:
-    print(course.get("code"))
+    registration_infos_response = requests.get(
+        url="https://acorn.utoronto.ca/sws/rest/enrolment/current-registrations",
+        cookies=cookies_dict,
+    )
+    registration_infos = registration_infos_response.text
+    registration_info_post_code = (
+        json.loads(registration_infos)[0].get("post").get("code")
+    )
+    registration_info_session_code = json.loads(registration_infos)[0].get(
+        "sessionCode"
+    )
+    data = {
+        "code": registration_info_session_code,
+        "posts": [
+            {
+                "code": registration_info_post_code,
+            }
+        ],
+    }
+    all_courses = {}
+    response = requests.post(
+        TIMETABLEAPI, headers=headers, data=json.dumps(data), cookies=cookies_dict
+    )
+    response = json.loads(response.text)
+    courses_infos = (
+        response.get("schedule").get("registrations")[0].get("academicActivitiesList")
+    )
+    if not only_course_code:
+        return courses_infos
+    for course in courses_infos:
+        course_codes_list.append(course.get("code"))
+    return course_codes_list
